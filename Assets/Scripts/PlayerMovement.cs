6a6bdbde9b8e35c58cbdcb7724b532951c17cdd9
BLecoffre <bertrandlecoffre@outlook.com>
2021-07-11 23:59:13 +0200

    Beginning of player controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed = 1.0f;
+    [SerializeField] private float gravity = -9.81f;
+
+    public Transform _groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask _groundMask;
+
+    Vector3 velocity;
+
+    private bool _isGrounded = false;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, groundDistance, _groundMask);
+
+        if (_isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
     }
 }
